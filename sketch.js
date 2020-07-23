c6ae33563e1d1a91c5a38d7fd0dc29401090ac43
VishalR24 <68307996+VishalR24@users.noreply.github.com>
2020-07-22 22:26:28 -0400

    updated sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -59,7 +59,7 @@ function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
 	Matter.Body.setStatic(packageBody, false)
-	restitution: 0
+	restitution: 0.5
   }
 }
 
